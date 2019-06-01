6cc7b4e9edb0a0d7ee01648ced17769df34b8fe7
Elizabeth Thomas-Cappello <ejd@elizabethjanedesigns.com>
2019-06-01 09:24:03 -0400

    Chapter 5 6-1-19

---
+++ src/ExerciseFive.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ethomascappello
+ */
+public class ExerciseFive {
+    public static void main (String[] args) {
+        System.out.print("(9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5) = ");
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    
+    
+}
+    
+}
